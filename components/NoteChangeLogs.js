1f09151fa394d8e9d75776e3f9fe7147d917b851
Dennis Oragon <dennis.pitallano@hotmail.co.uk>
2022-02-02 21:16:35 +0800

    add logs tables

--- components/NoteChangeLogs.js
+++ components/NoteChangeLogs.js
@@ -1,10 +1,106 @@
+import { useContext, useState } from "react";
+import { NotesContext } from "./App";
 
-function NoteChangeLogs(params) {
-    
+function NoteChangeLogs() {
+  const { notesData, noteChangeLogsData } = useContext(NotesContext);
+  const [selectedNoteId, setSelectedNoteId] = useState(-1);
+
+  const noteChangeLogsSelected = noteChangeLogsData.filter(
+    (rec) => rec.noteId === selectedNoteId
+  );
+
+  function dateOut(dateValue) {
+    return new Date(dateValue).toLocaleDateString("en", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  }
+
+  function NotesListBase() {
     return (
-        <div>Note Change logs </div>
+      <div className="col-6">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Date</th>
+              <th scope="col">Note Id</th>
+            </tr>
+          </thead>
+          <tbody>
+            {[...notesData]
+              .sort((a, b) => {
+                const dateA = a.createDate;
+                const dateB = b.createDate;
+                return dateA > dateB ? -1 : dateA < dateB ? 1 : 0;
+              })
+              .map(function (note) {
+                return (
+                  <tr
+                    key={note.id}
+                    className={note.id === selectedNoteId ? "selected-row" : ""}
+                    onClick={() => {
+                      setSelectedNoteId(note.id);
+                    }}
+                  >
+                    <th scope="row">
+                      <span className="cursor-pointer">{note.title}</span>
+                    </th>
+                    <td>{dateOut(note.createDate)}</td>
+                    <td>{`${note.id.slice(0, 6)}`}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
     );
-
+  }
+  function NoteListDetails() {
+    return (
+      <div className="col-6">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Change Date</th>
+              <th scope="col">Operation</th>
+              <th scope="col">Log Id</th>
+            </tr>
+          </thead>
+          <tbody>
+            {selectedNoteId != -1
+              ? noteChangeLogsSelected
+                  .sort((a, b) => {
+                    const dateA = a.changeDate;
+                    const dateB = b.changeDate;
+                    return dateA > dateB ? -1 : dateA < dateB ? 1 : 0;
+                  })
+                  .map(function (changeLogRec) {
+                    return (
+                      <tr>
+                        <td>{dateOut(changeLogRec.changeDate)}</td>
+                        <td>{changeLogRec.operation}</td>
+                        <td>{`${changeLogRec.id.slice(0,6)}...`}</td>
+                      </tr>
+                    );
+                  })
+              : null}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  return (
+    <div class="container">
+      <div className="row tab-content bg-transparent mt-4">
+        <NotesListBase />
+        <NoteListDetails />
+      </div>
+    </div>
+  );
 }
 
 export default NoteChangeLogs;
